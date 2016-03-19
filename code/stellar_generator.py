5fa748f95ccaef78bb569a8e9830689de17ddb75
TC Fraser <tcfraser237@gmail.com>
2016-03-19 16:36:38 -0400

    Better testing file

--- code/stellar_generator.py
+++ code/stellar_generator.py
@@ -1,11 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import rc
-
-# Computer modern fonts
-rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-rc('text', usetex=True)
-
 def test_fnc(a):
     print a
 
