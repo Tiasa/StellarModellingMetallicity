aa9a32be812712161945c8211603b6945a06b3c8
Tiasa Mondol <tmondol@uwaterloo.ca>
2016-03-19 18:38:42 -0400

    Fixed Kappa Power

--- code/getKappa.py
+++ code/getKappa.py
@@ -14,9 +14,9 @@ def kappa(X,Y,Z,rho,T):
     ## Electron Scattering Opacity
     kes = 0.2 * (1+X)
     ## Free-free Opacity
-    kff = 1e24 * (Z+0.0001) * (rho^0.7)*(T^-3.5)
+    kff = 1e24 * (Z+0.0001) * (rho**0.7)*(T**-3.5)
     ## Hydrogen opacity
-    kH = 2.5e-32 * (Z/0.02) * (rho^0.5) * (T^9)
+    kH = 2.5e-32 * (Z/0.02) * (rho**0.5) * (T**9)
     ## Kappa
     k = (1/kH)+(1/max(kff,kes))
     return k
