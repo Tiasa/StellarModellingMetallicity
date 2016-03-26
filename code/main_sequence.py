7cd893e3a2118db41c4a62921ce08223e0530a27
Tiasa Mondol <tmondol@uwaterloo.ca>
2016-03-26 18:37:26 -0400

    Main sequence

--- code/main_sequence.py
+++ code/main_sequence.py
@@ -1,12 +1,11 @@
 from __future__ import division, print_function
-from multiprocessing import Process, Lock
+from multiprocessing import Process, Lock, Array
 from constants import *
 from composition import Composition
 from rkf import rkf
 from bisection import bisection
 import matplotlib.pyplot as plt
 from stellar_generator import Star
-
 # Main sequence class 
 # USAGE : 
 #       ms = MainSequence(min_core_temp = $some temp$, max_core_temp = $some temp$, X = $some comp$, Y = $some comp$, num_of_stars = $some num$)
@@ -17,33 +16,49 @@ from stellar_generator import Star
 # OUTPUT:
 #       Plot !!!
 
+
 class MainSequence():
     def __init__(self, min_core_temp, max_core_temp,X,Y,num_of_stars):
         self.min_core_temp = min_core_temp
         self.max_core_temp = max_core_temp
         self.num_of_stars = num_of_stars
         self.composition = Composition.fromXY(X,Y)
-        self.temp = [0] * num_of_stars
         self.lumin = [0] * num_of_stars
-    def make_entry(self,lk,temp,lumin,ind):
-        lk.acquire()
-        self.temp[ind] = temp
-        self.lumin[ind] = lumin
-        print("Star {0} : Temp {1} : Lumin {2}".format(ind, temp, lumin))
-        lk.release()
-    def make_star(self,temp,lk,index):
-        star =  Star(temp_c = temp, composition=self.composition)
+        self.lock = Lock()
+    def make_entry(self,t,l,ind,temp,lumin):
+        self.lock.acquire()
+        temp[ind] = t
+        lumin[ind] = l/L_s
+        print("Star {0} : Temp {1} : Lumin {2} ".format(ind, t, l))
+        self.lock.release()
+    def make_star(self,t,index,temp,lumin):
+        star =  Star(temp_c = t, composition=self.composition)
         star.solve()
-         self.make_entry(lk,star.temp_surf,star.lumin_surf,index)
-    def calculate(self):
+        self.make_entry(star.temp_surf,star.lumin_surf,index,temp,lumin)
+    def calculate(self,temp,lumin):
         core_temp = np.linspace(start=self.min_core_temp,stop=self.max_core_temp,num=self.num_of_stars)
-        ## For synchromization creating a lock
-        lock = Lock()
+        procs = []
         for idx, t in enumerate(core_temp):
-             Process(target=self.make_star,args=(t,lock,idx)).start()
-        
-    #def plot(self):
-        # DO NOTHING FOR NOW
-        
+             p = Process(target=self.make_star,args=(t,idx,temp,lumin))
+             procs.append(p)
+             p.start()
+        for p in procs:
+            p.join()
+       
+    def plot(self):
+        ## For synchromization create process safe array
+        temp = Array("d",[0]*self.num_of_stars)
+        lumin = Array("d",[0]*self.num_of_stars)
+        self.calculate(temp,lumin)
+        tempArr = temp[:]
+        luminArr = lumin[:]
+        print (tempArr)
+        print (luminArr)
+        plt.figure()
+        plt.title("Main Sequence")
+        plt.xlabel("Temparature")
+        plt.ylabel("L/L_sun")
+        plt.plot(tempArr,luminArr)
+        plt.show()
 ms = MainSequence(min_core_temp=3.5e7,max_core_temp=9e5,X=0.73,Y=0.25,num_of_stars=10)
-ms.calculate()
+ms.plot()
